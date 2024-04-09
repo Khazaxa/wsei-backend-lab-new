7dbb3af3db5ece081de51cc75e69ed0e10c63cb1
Jakub Korban <jakub.korban@epoka.io>
2024-04-09 17:59:36 +0200

    zad1

--- WebApi/Mappers/AutoMapperProfiles.cs
+++ WebApi/Mappers/AutoMapperProfiles.cs
@@ -1,3 +1,4 @@
+using ApplicationCore.Models;
 using ApplicationCore.Models.QuizAggregate;
 using AutoMapper;
 using WebApi.Dto;
@@ -8,18 +9,29 @@ public class AutoMapperProfiles : Profile
 {
     public AutoMapperProfiles()
     {
-        CreateMap<QuizItem, QuizItemDto>()
-            .ForMember(
-                q => q.Options,
-                op => op.MapFrom(
-                    s => new List<string>(s.IncorrectAnswers) { s.CorrectAnswer }
-                )
-            );
-        CreateMap<Quiz, QuizDto>()
-            .ForMember(
-                t => t.Items,
-                op => op.MapFrom<List<QuizItem>>(
-                    i => i.Items
-            ));
+        CreateMap<QuizItemUserAnswer, AnswerDto>()
+            .ForMember(dest 
+                => dest.Question, opt 
+                => opt.MapFrom(src => src.QuizItem.Question))
+            .ForMember(dest 
+                => dest.Answer, opt 
+                => opt.MapFrom(src => src.Answer))
+            .ForMember(dest 
+                => dest.IsCorrect, opt 
+                => opt.MapFrom(src => src.IsCorrect()));
+
+        CreateMap<(int QuizId, int UserId, int TotalQuestions, IEnumerable<QuizItemUserAnswer> Feedback), FeedbackDto>()
+            .ForMember(dest 
+                => dest.QuizId, opt 
+                => opt.MapFrom(src => src.QuizId))
+            .ForMember(dest 
+                => dest.UserId, opt 
+                => opt.MapFrom(src => src.UserId))
+            .ForMember(dest 
+                => dest.TotalQuestions, opt 
+                => opt.MapFrom(src => src.TotalQuestions))
+            .ForMember(dest 
+                => dest.Answers, opt 
+                => opt.MapFrom(src => src.Feedback));
     }
 }
